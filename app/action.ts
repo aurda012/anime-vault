b59e3af0243d58111addf5450c199858f5b9010d
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-08 16:57:04 -0400

    Added server action to fetch anime data

---
+++ app/action.ts
@@ -0,0 +1,11 @@
+"use server";
+
+export const fetchAnime = async (page: number) => {
+  const res = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+
+  const data = await res.json();
+
+  return data;
+};
